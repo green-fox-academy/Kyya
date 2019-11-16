2464654d40a8cdc884117a514fbd322192579bd3
Anapopo <favoursmoe@gmail.com>
2019-11-16 16:45:50 +0800

    refactor(translator): finish gibberish translator

--- week-05/day-05/gibberish-translator-service/app.js
+++ week-05/day-05/gibberish-translator-service/app.js
@@ -5,24 +5,7 @@ const PORT = 8080;
 
 app.use(express.json());
 
-function translate(text) {
-  return text.split(' ').map(word => {
-    let i = Math.floor(Math.random() * word.length);
-    return word.substr(0, i) + 'bla' + word.substr(i);
-  }).join(' ');
-}
-
-app.post('/translate', (req, res) => {
-  const { text } = req.body;
-  if (text) {
-    res.send({
-      "translated": translate(text),
-      "lang": "gibberish"
-    })
-  } else {
-    res.send({ error: "I can't translate that!" })
-  }
-});
+app.post('/translate', require('./translate'));
 
 app.listen(PORT, () => {
   console.log(`The server is up and running on ${PORT}`);
