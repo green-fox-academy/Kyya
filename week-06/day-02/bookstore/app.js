d90c1a817be0df750584123e2417adf30113f940
Kyya <favoursmoe@gmail.com>
2019-11-19 16:29:14 +0800

    feat(bookstore): add filter for some fields

--- week-06/day-02/bookstore/app.js
+++ week-06/day-02/bookstore/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const conn = require('./connection').promise();
+const squel = require('squel');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -10,8 +11,38 @@ app.get('/books/title', async (req, res) => {
   res.render('index', { books: rows });
 })
 
-app.get('/books/list', async (req, res) => {
-  const [ rows ] = await conn.query('SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast INNER JOIN author USING(aut_id) INNER JOIN category USING(cate_id) INNER JOIN publisher USING(pub_id);');
+/*
+SELECT book_name, aut_name, cate_descrip, pub_name, book_price
+FROM book_mast
+INNER JOIN author USING(aut_id)
+INNER JOIN category USING(cate_id)
+INNER JOIN publisher USING(pub_id)
+*/
+app.get('/books', async (req, res) => {
+  const { category, publisher, pgt, plt } = req.query;
+
+  let query = squel
+    .select()
+    .from('book_mast', 'b')
+    .fields(['book_name', 'aut_name', 'cate_descrip', 'pub_name', 'book_price'])
+    .join('author', 'a', squel.expr().and('b.aut_id = a.aut_id'))
+    .join('category', 'c', squel.expr().and('b.cate_id = c.cate_id'))
+    .join('publisher', 'p', squel.expr().and('b.pub_id = p.pub_id'));
+
+  if (category) {
+    query = query.where(`cate_descrip = ?`, category);
+  }
+  if (publisher) {
+    query = query.where(`pub_name = ?`, publisher);
+  }
+  if (pgt) {
+    query = query.where(`book_price > ?`, pgt);
+  }
+  if (plt) {
+    query = query.where(`book_price < ?`, plt);
+  }
+
+  const [ rows ] = await conn.query(query.toString());
   res.render('table', { books: rows });
 })
 
