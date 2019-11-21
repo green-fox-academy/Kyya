7e3b1df74d36f98dfd39c5f5547c8a5871ea2f00
Anapopo <favoursmoe@gmail.com>
2019-11-21 22:28:06 +0800

    feat(chat-server): use dotenv

--- week-06/project/chat-server/app.js
+++ week-06/project/chat-server/app.js
@@ -1,10 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const messages = require('./routes/messages');
-
-const { config } = require('dotenv');
-config();
 
 app.use(cors());
 app.use(express.json());
@@ -18,6 +16,7 @@ app.get('/', (req, res) => {
 
 app.get('/messages', messages.getMessage);
 app.post('/messages', messages.addMessage);
+app.delete('/messages/:id', messages.deleteMessage);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, '0.0.0.0', () => console.log(`App listening on ${PORT}`));
