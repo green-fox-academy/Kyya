ed11b2b4334731451c28cad3a44075bf14434652
Anapopo <favoursmoe@gmail.com>
2019-11-21 22:27:20 +0800

    feat(chat-server): add delete message route

--- week-06/project/chat-server/routes/messages.js
+++ week-06/project/chat-server/routes/messages.js
@@ -34,5 +34,16 @@ module.exports = {
       res.sendStatus(500);
       throw new Error(error.message);
     }
-  }
+  },
+  deleteMessage: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const queryString = format('DELETE FROM message WHERE ?;', { id: parseInt(id) });
+      const [{ affectedRows }] = await conn.query(queryString);
+      res.sendStatus(affectedRows ? 204 : 404);
+    } catch (err) {
+      res.sendStatus(500);
+      throw new Error(err.message);
+    }
+  },
 }
