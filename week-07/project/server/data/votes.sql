bcff1ef95d62b40385df4facad5f0608eed0da5d
Kyya <favoursmoe@gmail.com>
2019-11-25 15:20:12 +0800

    feat(project/server): add triggers for vote

--- week-07/project/server/data/votes.sql
+++ week-07/project/server/data/votes.sql
@@ -7,3 +7,32 @@ CREATE TABLE IF NOT EXISTS Votes(
   FOREIGN KEY(uid) REFERENCES Users(id),
   FOREIGN KEY(pid) REFERENCES Posts(id)
 );
+
+CREATE TRIGGER After_Votes_Insert
+  AFTER INSERT ON Votes
+  FOR EACH ROW
+BEGIN
+  UPDATE Posts
+  SET score = score + NEW.type
+  WHERE Posts.id = NEW.pid;
+END;
+
+CREATE TRIGGER After_Votes_Delete
+  AFTER DELETE ON Votes
+  FOR EACH ROW
+BEGIN
+  UPDATE Posts
+  SET score = score - OLD.type
+  WHERE Posts.id = OLD.pid;
+END;
+
+CREATE TRIGGER After_Votes_Update
+  AFTER UPDATE ON Votes
+  FOR EACH ROW
+BEGIN
+  IF OLD.pid = NEW.pid THEN
+    UPDATE Posts
+      SET score = score - OLD.type + NEW.type
+      WHERE Posts.id = NEW.pid;
+  END IF;
+END;
