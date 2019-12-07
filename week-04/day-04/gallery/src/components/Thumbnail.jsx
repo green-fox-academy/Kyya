a32831510744f45fb0f06dac4e3e4631761fa7b8
Kyya <favoursmoe@gmail.com>
2019-12-07 17:53:41 +0800

    feat(gallery): split gallery into submodule

--- week-04/day-04/gallery/src/components/Thumbnail.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Thumbnail(props) {
-  return (
-    <div onClick={props.onClick} className={`thumbnail ${props.active?'active':''}`}>
-      <div style={{ backgroundImage: `url(${props.src})` }}></div>
-    </div>
-  )
-}
